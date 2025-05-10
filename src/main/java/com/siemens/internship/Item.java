bd357afa99f9432505849160051bdd2eb8ce77fd
DrNoris <noris.aurelian.net@gmail.com>
2025-05-10 19:56:21 +0300

    testing

--- src/main/java/com/siemens/internship/Item.java
+++ src/main/java/com/siemens/internship/Item.java
@@ -5,6 +5,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,13 +19,21 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Item {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotBlank(message = "Name is required")
+    @Size(max = 100, message = "Name must be at most 100 characters")
     private String name;
+
+    @Size(max = 255, message = "Description must be at most 255 characters")
     private String description;
+
     private String status;
 
-    // Add email regex validation
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
     private String email;
 }
